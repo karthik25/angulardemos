cf70ae80919129b8759da6458646e464fef3b9f2
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-09-23 00:09:47 -0400

    pkg dropdown

--- AngularDemos/Controllers/BasicController.cs
+++ AngularDemos/Controllers/BasicController.cs
@@ -16,7 +16,7 @@ namespace AngularDemos.Controllers
         [HttpGet]
         public List<Package> Packages()
         {
-            return Enumerable.Range(1, 20).Select(e => new Package { Id = e, Name = "Package " + e }).ToList();
+            return Enumerable.Range(21, 40).Select(e => new Package { Id = e, Name = "Package " + e }).ToList();
         }
     }
 }
