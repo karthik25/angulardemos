6d543394b373e6e5a53cfb12585fea3e01f68e35
Karthik Ananthapadmanaban <karthika25@gmail.com>
2014-09-23 22:40:49 -0400

    refactored the cb list rel. code

--- AngularDemos/Controllers/BasicController.cs
+++ AngularDemos/Controllers/BasicController.cs
@@ -1,22 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using AngularDemos.Scripts;
+using AngularDemos.Models;
 
 namespace AngularDemos.Controllers
 {
     public class BasicController : ApiController
     {
         [HttpGet]
-        public List<Client> Clients()
+        public List<CheckBoxEntry> List1()
         {
-            return Enumerable.Range(1, 20).Select(e => new Client { Id = e, Name = "Client " + e }).ToList();
+            return Enumerable.Range(1, 20).Select(e => new CheckBoxEntry { Id = e, Name = "Citem " + e }).ToList();
         }
 
         [HttpGet]
-        public List<Package> Packages()
+        public List<CheckBoxEntry> List2()
         {
-            return Enumerable.Range(21, 40).Select(e => new Package { Id = e, Name = "Package " + e }).ToList();
+            return Enumerable.Range(21, 40).Select(e => new CheckBoxEntry { Id = e, Name = "Pitem " + e }).ToList();
         }
     }
 }
---
+++ AngularDemos/Models/CheckBoxEntry.cs
@@ -0,0 +1,8 @@
+﻿namespace AngularDemos.Models
+{
+    public class CheckBoxEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
